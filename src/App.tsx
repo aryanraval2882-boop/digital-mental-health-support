b0e96175947a4a746953bfd98c9346f15acc849b
Aryan Raval <aryanraval2882@gmail.com>
2025-11-09 18:56:48 +0530

    changed build config

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/stress-less-studylife/">
+      <BrowserRouter basename="/digital-mental-health-support/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/stress-less-studylife/",
+  base: "/digital-mental-health-support/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
